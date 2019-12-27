f5176122285a760925aa99512d6ac86e8747ddc4
niuyuxian <ncc0706@gmail.com>
2019-12-27 21:18:25 +0800

    hello heroku

---
+++ src/test/java/io/arukas/SpringBootHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arukas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
